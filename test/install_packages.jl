0b490ac40acbae348c668a23c8f28b68748415d9
Ho-Chun Lin <44913081+hochunlin@users.noreply.github.com>
2024-05-30 23:21:37 -0700

    Constrain GeometryPrimitives and Makie to a specific version

--- test/install_packages.jl
+++ test/install_packages.jl
@@ -1,8 +1,8 @@
 using Pkg
 
 Pkg.add("Arpack")
-Pkg.add(url="https://github.com/stevengj/GeometryPrimitives.jl") # installing the latest version of GeometryPrimitives, which fixed its dependency error 
-Pkg.add("LinearAlgebra")
+Pkg.add(Pkg.PackageSpec(name="Makie", version="0.19.12")) # Registered GeometryPrimitives (v0.4.6) is only compatible with the old version Makie (v0.19.12)
+Pkg.add("GeometryPrimitives") # (v0.4.6)Pkg.add("LinearAlgebra")
 Pkg.add("Plots")
 Pkg.add("Printf")
 Pkg.add("Random")
